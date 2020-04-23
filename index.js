48c2d20d5b2c8395a1ff0c27963ae94d630bc5f7
Muhid Hossain <muhidhossain7@gmail.com>
2020-04-24 02:23:32 +0600

    env and gitignore added

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+const uri = process.env.DB_PATH;
+
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+app.get('/foodItems', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("redOnion").collection("foodItems");
+        collection.find().toArray((err, documents)=>{
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(documents);
+            }
+        })
+        client.close();
+      });
+})
+
+app.post('/addFoodItems', (req, res) => {
+    const foodItems = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("redOnion").collection("foodItems");
+        collection.insertOne({}, (err, result) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message:err});
+            }
+            else{
+                res.send(result.ops[0]);
+            };
+        })
+        client.close();
+    });
+});
+
+app.listen(4000, () => console.log('Listening 4000'));
